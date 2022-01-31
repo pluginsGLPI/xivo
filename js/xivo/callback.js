4b04895c39bf2cc1bd0b65ff30d3ed4d34c55783
Cédric Anne <cedric.anne@gmail.com>
2022-01-31 16:20:25 +0100

    Fix licence headers

--- js/xivo/callback.js
+++ js/xivo/callback.js
@@ -1,3 +1,31 @@
+/**
+ * -------------------------------------------------------------------------
+ * xivo plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of xivo.
+ *
+ * xivo is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * xivo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xivo. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2017-2022 by xivo plugin team.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/pluginsGLPI/xivo
+ * -------------------------------------------------------------------------
+ */
+
 var Callback = {
 
 
